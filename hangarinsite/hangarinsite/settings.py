f5d98f1ddcb3aa1b8e4427f744e9f09b2aff208b
re_tricks <202280064@psu.palawan.edu.ph>
2025-10-21 18:50:53 +0800

    added github login

--- hangarinsite/hangarinsite/settings.py
+++ hangarinsite/hangarinsite/settings.py
@@ -159,7 +159,3 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = [
-    'http://127.0.0.1:8000',
-    'http://localhost:8000'
-]
